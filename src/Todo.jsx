ddcee2b36c21a555f45226fb7c3724ac81d55449
taiyow <taiyo.w@gmail.com>
2024-09-10 23:02:54 +0900

    "54. JSXで構造を作成" を実装

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -2,8 +2,31 @@ import { useState } from 'react'
 
 export const Todo = () => {
   return (
-    <div>
-      <h1>Hello, world</h1>
-    </div>
+    <>
+      <div>
+        <input placeholder='TODOを入力' />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のTODO</p>
+        <ul>
+          <li>
+            <p>TODOです</p>
+            <button>完了</button>
+            <button>削除</button>
+          </li>
+        </ul>
+      </div>
+      <div>
+      <p>完了のTODO</p>
+        <ul>
+          <li>
+            <p>TODOでした</p>
+            <button>戻す</button>
+          </li>
+        </ul>
+
+      </div>
+    </>
   )
 }
