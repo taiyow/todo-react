9fb608b49f5dd428e18f762e2a0b8be74b5d6df8
taiyow <taiyo.w@gmail.com>
2024-09-11 22:58:58 +0900

    "59. タスクの完了機能" の実装

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -29,6 +29,15 @@ export const Todo = () => {
     setIncompleteTodos(newTodos);
   };
 
+  const onClickComplete = (index) => {
+    const newIncompleteTodos = [...incompleteTodos];
+    newIncompleteTodos.splice(index, 1);
+    setIncompleteTodos(newIncompleteTodos);
+    const newCompleteTodos = [...completeTodos];
+    newCompleteTodos.push(incompleteTodos[index])
+    setCompleteTodos(newCompleteTodos);
+  };
+
   return (
     <>
       <div className="input-area">
@@ -42,7 +51,7 @@ export const Todo = () => {
             <li key={todo}>
               <div className="list-row">
                 <p className="todo-item">{todo}</p>
-                <button>完了</button>
+                <button onClick={() => onClickComplete(index)}>完了</button>
                 <button onClick={() => onClickDelete(index)}>削除</button>
               </div>
             </li>
